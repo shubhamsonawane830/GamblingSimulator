68163d559386e974d4881edf7256ca8051873ee4
shubhamsonawane830 <shubhamsonawane830@gmail.com>
2020-12-03 12:56:39 +0530

    Added: Calculate Won Loss

--- GamblingSimulator/src/com/GamblingSimulation/GamblingSimulation.java
+++ GamblingSimulator/src/com/GamblingSimulation/GamblingSimulation.java
@@ -7,22 +7,24 @@ public class GamblingSimulation {
 	public static final int STAKE = 100;
 	public static final int BET = 1;
 	public static final float STAKE_VALUE = 0.5f;
-	public int stake=100, loosingAmount, winningAmount, totalAmountEarned = 0;
+	public static int stake=100;
+	public int loosingAmount;
+	public int winningAmount;
+	public int totalAmountEarned = 0;
+	public int LostDays=0,WonDays=0;
 	
 	public int winOrLoss() {
 		Random random = new Random();
 		int value = random.nextInt(2);
 		if(value == 1) {
-			System.out.println("*GAMBLER WON*");
-			stake++ ;
+			stake++;
 		}else {
-			System.out.println("*GAMBLER LOST*");
-			stake-- ;
+			stake--;
 		}
 		return stake;
-	}
+	  }
 	
-	public int resignStake() {
+	public Integer resignStake(int day) {
 		loosingAmount = (int)Math.round(STAKE * STAKE_VALUE);
 		winningAmount = (int)Math.round(STAKE + (STAKE * STAKE_VALUE));
 		boolean stop = true;
@@ -30,28 +32,38 @@ public class GamblingSimulation {
 		while(stop == true) {
 			winOrLoss();
 			if(stake == loosingAmount) {
+				LostDays++;
 				stop = false;
 			}
 			if(stake == winningAmount){
+				WonDays++;
 				stop = false;
 			}
 		}
 		return stake;
-	}
+	  }
 	
-	public int getTotalAmountWonOrLoss() {
+	public int getTotalAmountWonOrLoss(){
         int day_stake=0;
-        for(int day=0;day<=20;day++) {
-            day_stake=resignStake();
+        for(int day=1;day<=20;day++) {
+            day_stake=resignStake(day);
+            System.out.println("Day: "+day+" amount: "+day_stake);
             totalAmountEarned=totalAmountEarned+day_stake;
         }
-        System.out.println("Total Amount Earned or Loss by Gambler at End of 20 days:- " + totalAmountEarned);
+        System.out.println("Total Amount Earned or Loss by Gambler at End of given period:- " + totalAmountEarned);
+        return totalAmountEarned;
+       }
+	
+	public int calculateForMonth() {
+        getTotalAmountWonOrLoss();
+        System.out.println("Days Won in  month:"+WonDays);
+        System.out.println("Days Loss in  month:"+LostDays);
         return totalAmountEarned;
     }
-
+	
 	public static void main(String[] args) {
 		System.out.println("*WELCOME TO GAMBLING SIMULATION PROBLEM*");
 		GamblingSimulation gambling = new GamblingSimulation();
-		gambling.getTotalAmountWonOrLoss();
+		gambling.calculateForMonth();
 	}
 }
