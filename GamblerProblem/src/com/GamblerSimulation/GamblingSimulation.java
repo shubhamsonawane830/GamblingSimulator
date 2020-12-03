8e0f7009e0ca96273ca150425c142f899b0323a9
shubhamsonawane830 <shubhamsonawane830@gmail.com>
2020-12-03 19:28:50 +0530

    Added: Gambling Win or loss

--- GamblerProblem/src/com/GamblerSimulation/GamblingSimulation.java
+++ GamblerProblem/src/com/GamblerSimulation/GamblingSimulation.java
@@ -1,10 +1,38 @@
 package com.GamblerSimulation;
 
+import java.util.Random;
+import java.math.*;
+
 public class GamblingSimulation {
+	
+	static int gambler = 100;
+	public static final int win = 1;
+	public static final int lose = 0;
+	public void 	bet ()
+	{
+		gambler = gambler - 1;
+		int bet =  (int)Math.round(Math.random());
+		if(bet == win)
+		{
+			gambler = gambler + 2 ;
+			System.out.println("Gambler won the bet");
+			System.out.println("Gambler Money : "+gambler+"$");
+		}
+		else
+		{
+			System.out.println("Gambler lost the bet");
+			System.out.println("Gambler Money : "+gambler+"$");
+		}
+	}
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		GamblingSimulation gamble = new GamblingSimulation();
+		gamble.bet();
+	}
 
 	}
 
-}
+
